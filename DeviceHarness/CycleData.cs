cd53409f85f13f8f1d8ac34fc7783c7a72443346
Mark Mydland <markmyd@microsoft.com>
2019-01-12 18:26:31 -0800

    update comments and add license statements

--- DeviceHarness/CycleData.cs
+++ DeviceHarness/CycleData.cs
@@ -1,3 +1,14 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,12 +17,25 @@ using Microsoft.Azure.Devices.Client;
 
 namespace DeviceHarness
 {
+    /// <summary>
+    /// Class to parse and model a single row of device data found in the
+    /// datasets.
+    /// </summary>
     public class CycleData
     {
         private readonly Dictionary<string, object> Columns = new Dictionary<string, object>();
 
+        /// <summary>
+        ///  Serializes the CycleData into a message in JSON format to be sent to IoT Hub
+        /// </summary>
+        /// <returns>JSON serialized message to send to IoT Hub</returns>
         public string Message => JsonConvert.SerializeObject(Columns);
 
+        /// <summary>
+        /// Public constructor takes an array of string representing a row of 
+        /// data from a data set file
+        /// </summary>
+        /// <param name="cycleDataRow">An array of string representing a row of data from a dataset</param>
         public CycleData(string[] cycleDataRow)
         {
             if (cycleDataRow.Length != 26)
--- DeviceHarness/TurbofanDevice.cs
+++ DeviceHarness/TurbofanDevice.cs
@@ -1,3 +1,14 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Client;
 using System;
@@ -9,18 +20,28 @@ using Message = Microsoft.Azure.Devices.Client.Message;
 
 namespace DeviceHarness
 {
+    /// <summary>
+    /// Simulates the physical device connected to the IoT Hub.
+    /// </summary>
     public class TurbofanDevice
     {
         private static readonly TimeSpan CycleTime = TimeSpan.FromSeconds(1);
-        
         private readonly int deviceUnitNumber;
         private readonly string deviceConnectionString;
-        private readonly  TrainingFileManager trainingFileManager;
-        
+        private readonly TrainingFileManager trainingFileManager;
         private int messagesSent = 0;
 
+        /// <summary>
+        /// The total number of messages sent by this device to IoT Hub
+        /// </summary>
         public int MessagesSent => messagesSent;
 
+        /// <summary>
+        /// Public constructor for TurbofanDevice class.
+        /// </summary>
+        /// <param name="deviceNumber">Integer number of the data series device to send</param>
+        /// <param name="iotHubConnectionString">Connection string for the IoT Hub in which to create the device</param>
+        /// <param name="fileManager">TrainingFileManager for the dataset to be read and sent</param>
         public TurbofanDevice(int deviceNumber, string iotHubConnectionString, TrainingFileManager fileManager)
         {
             deviceUnitNumber = deviceNumber;
@@ -29,12 +50,22 @@ namespace DeviceHarness
             trainingFileManager = fileManager;
         }
 
+        /// <summary>
+        /// Creates an asynchronous task for sending all data for the device.
+        /// </summary>
+        /// <returns>Task for asynchronous device operation</returns>
         public async Task RunDeviceAsync()
         {
             List<CycleData> cycleData = LoadDeviceData();
             await SendDataToHub(cycleData).ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Takes a set of CycleData for a device in a dataset and sends the
+        /// data to the message with a configurable delay between each message
+        /// </summary>
+        /// <param name="cycleData">The set of data to send as messages to the IoT Hub</param>
+        /// <returns></returns>
         private async Task SendDataToHub(List<CycleData> cycleData)
         {
             DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
@@ -45,6 +76,13 @@ namespace DeviceHarness
             }
         }
 
+        /// <summary>
+        /// Uses the IoT Hub connection string to try to retrieve the given device ID. If the
+        /// device does not exist in the IoT Hub the method creates the device in the IoT Hub
+        /// </summary>
+        /// <param name="iotHubConnectionString">Connection string for the IoT Hub</param>
+        /// <param name="deviceId">Name of the device in the IoT Hub e.g Device_001</param>
+        /// <returns></returns>
         private async Task<string> CreateIotHubDevice(string iotHubConnectionString, string deviceId)
         {
             RegistryManager regManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
@@ -61,15 +99,27 @@ namespace DeviceHarness
             return $"HostName={hostname};DeviceId={device.Id};SharedAccessKey={device.Authentication.SymmetricKey.PrimaryKey}";
         }
 
+        /// <summary>
+        ///  Using the trainingFileManager for this instance read the device data for this device
+        /// </summary>
+        /// <returns>List representing all cycle data for this device</returns>
         private List<CycleData> LoadDeviceData()
         {
             return trainingFileManager.ReadDeviceData(deviceUnitNumber);
         }
 
+        
+        /// <summary>
+        /// Uses the DeviceClient to send a message to the IoT Hub
+        /// </summary>
+        /// <param name="deviceClient">Azure Devices client for connecting to and send data to IoT Hub</param>
+        /// <param name="message">JSON string representing serialized device data</param>
+        /// <returns>Task for async execution</returns>
         private async Task SendEvent(DeviceClient deviceClient, string message)
         {
             using (Message eventMessage = new Message(Encoding.UTF8.GetBytes(message)))
             {
+                // Set the content type and encoding so the IoT Hub knows to treat the message body as JSON
                 eventMessage.ContentEncoding = "utf-8";
                 eventMessage.ContentType = "application/json";
 
