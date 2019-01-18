aad0e1e4de135469a75d329151edef04715fcd1f
David R. Williamson <drwill@microsoft.com>
2019-01-18 14:23:52 -0800

    Migrate LeafDevice files

---
+++ LeafDevice/program.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Microsoft.Azure.IoT.Samples
+{
+    class Program
+    {
+        private const int CycleTimeSeconds = 1;
+
+        private static string connectionString;
+        private static CommandLineApplication app = new CommandLineApplication();
+        private static CommandOption connectionStringOption;
+        private static CommandOption certificateOption;
+
+        private static void Main(string[] args)
+        {
+            InitializeApp();
+
+            app.OnExecute(() =>
+            {
+                GetConnectionString();
+                InstallCertificate();
+                SensorArray sensorArray = new SensorArray();
+                DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+                foreach (var cycle in sensorArray.Cycles)
+                {
+                    SendEvent(deviceClient, cycle.Message).Wait();
+                }
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+
+        private static void InstallCertificate()
+        {
+            string certificatePath;
+            if (!certificateOption.HasValue())
+            {
+                certificatePath = Environment.GetEnvironmentVariable("CA_CERTIFICATE_PATH");
+            }
+            else
+            {
+                certificatePath = certificateOption.Value();
+            }
+
+            if (!String.IsNullOrWhiteSpace(certificatePath))
+            {
+                CertificateManager.InstallCACert(certificatePath);
+            }
+        }
+
+        private static void InitializeApp()
+        {
+            app.Name = "LeafDevice";
+            app.Description = "Demo leaf device";
+
+            app.HelpOption("-?|-h|--help");
+
+            connectionStringOption = app.Option(
+                "-x|--connection",
+                "Device connection string e.g. HostName=yourHub.azure-devices.net;DeviceId=yourDevice;SharedAccessKey=XXXYYYZZZ=;GatewayHostName=mygateway.contoso.com",
+                CommandOptionType.SingleValue);
+
+            certificateOption = app.Option(
+                "-c|--certificate",
+                "Certificate with root CA in PEM format",
+                CommandOptionType.SingleValue);
+        }
+
+        private static void GetConnectionString()
+        {
+            if (!connectionStringOption.HasValue())
+            {
+                connectionString = Environment.GetEnvironmentVariable("DEVICE_CONNECTION_STRING");
+                app.ShowHint();
+            }
+            else
+            {
+                connectionString = connectionStringOption.Value();
+            }
+
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                app.ShowHint();
+                throw new ArgumentException();
+            }
+
+            Console.WriteLine($"Using connection string: {connectionString}");
+        }
+
+        static async Task SendEvent(DeviceClient deviceClient, string message)
+        {
+            Console.WriteLine($"{DateTime.Now} > Sending message: {message}");
+            Message eventMessage = new Message(Encoding.ASCII.GetBytes(message));
+            await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            await Task.Delay(TimeSpan.FromSeconds(CycleTimeSeconds)).ConfigureAwait(false);
+        }
+    }
+}
