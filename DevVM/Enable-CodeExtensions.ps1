562146542d1249b6a78e091fac8ce0769e02e325
Mark Mydland <markmyd@microsoft.com>
2019-01-22 23:26:13 +0000

    remove azure account extension since it is installed from the azure iot tools anyway (#20)

--- DevVM/Enable-CodeExtensions.ps1
+++ DevVM/Enable-CodeExtensions.ps1
@@ -4,9 +4,6 @@ code --install-extension vsciot-vscode.azure-iot-tools
 #python extensions
 code --install-extension ms-python.python
 
-#azure extension
-code --install-extension ms-vscode.azure-account
-
 #dotnet extensions
 code --install-extension ms-vscode.csharp
 
